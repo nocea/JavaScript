b8f2bb390a330b11e1fb5c9456a90128ebc3965c
mnocea <mnoceacabrera@gmail.com>
2023-02-06 09:23:49 +0100

    Cambis

--- edu.javaScript.introduccionClase/src/main/webapp/script/primerasLetras2.js
+++ edu.javaScript.introduccionClase/src/main/webapp/script/primerasLetras2.js
@@ -1,6 +1,6 @@
 function primerasLetras(texto,caracter){
 	let text = texto;
 	let letra = text.charAt(caracter);
-	document.write(texto);
-	document.write(letra);	
+	
+	document.write("Letra: "+letra);	
 }
