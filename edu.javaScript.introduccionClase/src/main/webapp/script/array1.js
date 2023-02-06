b4bafb07949dbd8304148c8fca0e39cd82b5b042
mnocea <mnoceacabrera@gmail.com>
2023-02-06 12:38:38 +0100

    array

--- edu.javaScript.introduccionClase/src/main/webapp/script/array1.js
+++ edu.javaScript.introduccionClase/src/main/webapp/script/array1.js
@@ -38,8 +38,8 @@ function concatenar1(){
 	document.write(textos[0]+textos[1]+textos[2]);
 }
 function concatenar2(){
-	let partes = ["posiciones", "se cuentan", "Las", "array", "cero.", "a partir", "del"];
-	document.write(partes[2]+" "+partes  [0]+" "+partes[6]+" "+partes[3]+" "+partes[1]+" "+partes[5]+" "+partes[6]+" "+partes[4]);
+	let partes = ["posiciones ", "se cuentan ", "Las ", "array ", "cero.", "a partir ", "del "];
+	document.write(partes[2]+partes  [0]+partes[6]+partes[3]+partes[1]+partes[5]+partes[6]+partes[4]);
 }
 
 
