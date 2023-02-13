c6ca0dd2cbb795b29f3558c5dc1cc4858aa1a170
mnocea <mnoceacabrera@gmail.com>
2023-02-13 10:13:14 +0100

    Ejercicio1

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/codigo.js
@@ -0,0 +1,8 @@
+//Escribe en la consola 
+console.log("Hola Mundo");
+//Manda una alerta justo al iniciar la vista
+alert("Active el soporte JavaScript si aun no lo tiene activado");
+//Función que hace que se escriba en la vista una frase.
+function primerScript(){
+	document.write("Soy el primer Script")
+}
